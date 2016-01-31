d59bdbc5bd5e1d2d50a0496fdca6936b922dcd1e
Sherman Adelson <sherman.adelson@gmail.com>
2016-01-31 18:24:36 -0500

    added desktop app installs

---
+++ desktop/install-i3.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# install i3 window manager
+# https://i3wm.org/docs/repositories.html
+sudo echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get --allow-unauthenticated install sur5r-keyring
+sudo apt-get update
+sudo apt-get install i3
