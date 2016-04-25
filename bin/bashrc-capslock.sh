e7fa9baedb8ca9fcbcc0a996f444f65f24fcb906
Mnebuerquo <sherman.adelson@gmail.com>
2016-04-24 21:00:58 -0400

    added bash os detection for capslock script

---
+++ bin/bashrc-capslock.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ "$OSTYPE" == "darwin"* ]; then
+	# use xmodmap to install the capslock mapping
+	xmodmap ~/.i3/xmodmap &> /dev/null
+	# turn off capslock if it's on already
+	~/bin/CAPSLOCKOFF
+fi
