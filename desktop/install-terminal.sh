b7242e26cbbdfe402641b0605334a78e5db4bc33
Mnebuerquo <sherman.adelson@gmail.com>
2016-05-04 10:43:11 -0400

    added script to change terminal to sakura

---
+++ desktop/install-terminal.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# I got tired of gnome-terminal
+# It's really a pain in the ass to configure
+
+# better terminal than gnome!
+sudo apt-get install sakura
+
+sudo update-alternatives --config x-terminal-emulator
