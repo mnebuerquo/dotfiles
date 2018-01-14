fb3fed025720fb0e206833fc83837a3bcbec4f35
mnebuerquo <sherman.adelson@gmail.com>
2018-01-14 11:01:53 -0500

    added cmake to vim install

--- install-scripts/mac/install-vim.sh
+++ install-scripts/mac/install-vim.sh
@@ -2,13 +2,16 @@
 
 brew update
 
+# I don't like macvim, and regular vim works better for me, also it's more
+# up to date than macvim
 #brew tap macvim-dev/macvim
 #brew install --HEAD macvim-dev/macvim/macvim
-
-brew install vim 
 #brew install macvim
-
 #brew link macvim
+
+brew install vim
+
+brew install cmake # for YouCompleteMe
 
 brew install the_silver_searcher
 
