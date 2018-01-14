cec8e740acaebf5304e84950b4be565c39a1fd75
mnebuerquo <sherman.adelson@gmail.com>
2018-01-14 10:46:00 -0500

    improved comments on git-name.sh

--- bin/git-name.sh
+++ bin/git-name.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if git remote -v | grep "spatial" ; then
-	echo "sherman"
+	echo "spatial"
 	git config user.name "sherman"
 	git config user.email "sherman@spatial.ai"
 	git config user.username "sherman"
 else
-	echo "other"
+	echo "home"
 	git config user.name "mnebuerquo"
 	git config user.email "sherman.adelson@gmail.com"
 	git config user.username "mnebuerquo"
