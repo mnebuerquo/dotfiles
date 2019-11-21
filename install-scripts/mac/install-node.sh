0648ee69c15971e0f0cff3805eaebdba0d0bfd69
mnebuerquo <mnebuerquo@users.noreply.github.com>
2019-11-21 10:09:57 -0500

    updated node installer

--- install-scripts/mac/install-node.sh
+++ install-scripts/mac/install-node.sh
@@ -2,11 +2,17 @@
 
 set -e
 
-# https://github.com/nvm-sh/nvm#install--update-script
+#https://github.com/nvm-sh/nvm#install--update-script 
 NVM_VERSION="v0.35.1"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/"${NVM_VERSION}"/install.sh > downloaded-nvm-install.sh
-chmod +x downloaded-nvm-install.sh
-./downloaded-nvm-install.sh --no-use
+NVM_INSTALLER="downloaded-nvm-install.sh"
+echo "Installing NVM. See https://github.com/nvm-sh/nvm#install--update-script for more info."
+echo "curl https://raw.githubusercontent.com/creationix/nvm/"${NVM_VERSION}"/install.sh | bash"
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/"${NVM_VERSION}"/install.sh > "${NVM_INSTALLER}"
+chmod +x "${NVM_INSTALLER}"
+bash "${NVM_INSTALLER}" --no-use
+rm "${NVM_INSTALLER}"
 
 # https://github.com/yarnpkg/yarn/issues/3255
+echo "Installing yarn for use with NVM. See https://github.com/yarnpkg/yarn/issues/3255 for more info."
 brew install yarn --without-node
